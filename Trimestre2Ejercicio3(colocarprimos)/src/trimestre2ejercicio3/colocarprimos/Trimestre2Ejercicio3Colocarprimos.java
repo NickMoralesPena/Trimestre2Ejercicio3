a7c4694ce61f729ea6907751473f0c7f72a91077
Nick Morales <nick.morales@cesjuanpablosegundo.es>
2019-02-01 13:52:47 +0100

    Primera versión del Trimestre2Ejercicio3

---
+++ Trimestre2Ejercicio3(colocarprimos)/src/trimestre2ejercicio3/colocarprimos/Trimestre2Ejercicio3Colocarprimos.java
@@ -0,0 +1,18 @@
+
+package trimestre2ejercicio3.colocarprimos;
+
+/**
+ *
+ * @author Usuario DAM 1
+ */
+public class Trimestre2Ejercicio3Colocarprimos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        
+    }
+    
+}
